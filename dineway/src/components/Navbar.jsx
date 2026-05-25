3d36614848d8386b7982505af040bef68b9bbcc8
anneline-codes <lineanne65@gmail.com>
2026-05-25 19:18:05 +0200

    Some changes were made

--- dineway/src/components/Navbar.jsx
+++ dineway/src/components/Navbar.jsx
@@ -1,28 +1,10 @@
 import logo from '../assets/logo.png'
+import styles from './Navbar.module.css'
 
 export default function Navbar({ logoClass = '' }) {
   return (
-    <nav style={styles.nav}>
-      <img src={logo} alt="Dineway Restaurant Solutions" style={styles.logo} className={logoClass} />
+    <nav className={styles.nav}>
+      <img src={logo} alt="Dineway Restaurant Solutions" className={`${styles.logo} ${logoClass}`} />
     </nav>
   )
-}
-
-const styles = {
-  nav: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    display: 'flex',
-    justifyContent: 'center',
-    padding: '40px 40px 0',
-    zIndex: 10,
-  },
-  logo: {
-    height: '140px',
-    objectFit: 'contain',
-    filter: 'drop-shadow(0 2px 8px rgba(0,0,0,0.5))',
-    marginTop: '20px',
-  },
 }
